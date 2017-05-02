a73b9c058feb08bf3febf31d71fb52448be48e9c
Jefferson Henrique Ramos <jeff.hen0811@gmail.com>
2017-05-01 23:47:39 -0300

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
 
   def hello
     render html: "<h1>hello, world!</h1>".html_safe
